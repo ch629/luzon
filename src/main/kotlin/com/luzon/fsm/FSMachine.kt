d10adc24af415f9d647c4d3a668ad04fee243d6a
ch629 <CharlieHowes97@outlook.com>
2018-07-12 22:35:53 +0100

    Added output alphabet type. Modified transitions to use a list of pairs rather than a map, so multiple transitions with the same predicate can be used (Primarily Epsilon). Add transition methods added to State.

--- src/main/kotlin/com/luzon/fsm/FSMachine.kt
+++ src/main/kotlin/com/luzon/fsm/FSMachine.kt
@@ -1,37 +1,36 @@
 package com.luzon.fsm
 
-data class FSMachine(var states: List<State>) { //TODO: Output Alphabet
-    constructor(root: State) : this(listOf(root))
+data class FSMachine<T>(var states: List<State<T>>) {
+    constructor(root: State<T>) : this(listOf(root))
 
     companion object {
-        fun fromRegex(str: String): FSMachine {
+        fun <T> fromRegex(str: String): FSMachine<T> {
             TODO()
         }
     }
 
     fun accept(char: Char) {
-        var newStates = emptyList<State>()
+        var newStates = emptyList<State<T>>()
         states.forEach { newStates += it.accept(char) }
         states = newStates
     }
 
-    fun merge(other: FSMachine): FSMachine { //TODO: Merge multiple Regex to the correct output letter
+    fun merge(other: FSMachine<T>): FSMachine<T> { //TODO: Merge multiple Regex to the correct output letter
         TODO("Merge two FSM together, merging the same regex to lead to an end result (Good for numerical literals, because the only difference between a double and a float is the f on the end.")
     }
 }
 
-data class State(val accept: Boolean = false) {
-    private val transitions = mapOf<(Char) -> Boolean, State>()
+class State<T>(val output: T? = null) {
+    private var transitions = emptyList<Pair<(Char) -> Boolean, State<T>>>()
 
-    fun accept(char: Char, containsEpsilons: Boolean = false): List<State> {
-        var newStates = transitions.entries.filter { it.key(char) }.map { it.value }.toList()
-        var epsilonStates = emptyList<State>()
+    fun accept(char: Char, containsEpsilons: Boolean = false): List<State<T>> {
+        var newStates = transitions.filter { it.first(char) }.map { it.second }.toList()
+        var epsilonStates = emptyList<State<T>>()
         if (containsEpsilons) {
             newStates.forEach {
-                val epsilonTransitions = it.transitions.filterKeys { it == epsilon }.map { it.value }
+                val epsilonTransitions = it.transitions.filter { it.first == epsilon }.map { it.second }
 
-                if (epsilonTransitions.isNotEmpty())
-                    epsilonStates += epsilonTransitions
+                epsilonStates += epsilonTransitions
             }
             newStates += epsilonStates
         }
@@ -39,9 +38,17 @@ data class State(val accept: Boolean = false) {
         return newStates
     }
 
-    //TODO: onEnter, onLeave etc
+    fun addTransition(pred: (Char) -> Boolean, state: State<T>) {
+        transitions += pred to state
+    }
+
+    fun addEpsilonTransition(state: State<T>) {
+        transitions += epsilon to state
+    }
+
+    fun isAccepting() = output != null
+
+    //TODO: onEnter, onLeave etc? -> Might not use this really.
 }
 
-val epsilon: (Char) -> Boolean = {
-    true
-}
+val epsilon: (Char) -> Boolean = { true }
