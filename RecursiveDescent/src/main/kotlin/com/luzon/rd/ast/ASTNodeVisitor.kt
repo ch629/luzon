b473fbb321ef3b77b04855e8fe880a500b0ed44d
ch629 <CharlieHowes97@outlook.com>
2019-02-10 14:06:40 +0000

    Added a generic return type to the generated visitors.

--- RecursiveDescent/src/main/kotlin/com/luzon/rd/ast/ASTNodeVisitor.kt
+++ RecursiveDescent/src/main/kotlin/com/luzon/rd/ast/ASTNodeVisitor.kt
@@ -1,79 +1,77 @@
 package com.luzon.rd.ast
 
-interface ASTNodeVisitor {
-    fun visit(node: ASTNode.Class)
-    fun visit(node: ASTNode.Constructor)
-    fun visit(node: ASTNode.ConstructorVariableDeclaration)
-    fun visit(node: ASTNode.FunctionDefinition)
-    fun visit(node: ASTNode.FunctionParameter)
-    fun visit(node: ASTNode.ForLoop)
-    fun visit(node: ASTNode.WhileLoop)
-    fun visit(node: ASTNode.IfStatement)
-    fun visit(node: ASTNode.ElseStatements.ElseIfStatement)
-    fun visit(node: ASTNode.ElseStatements.ElseStatement)
-    fun visit(node: ASTNode.VariableDeclaration)
-    fun visit(node: ASTNode.VariableAssign)
-    fun visit(node: ASTNode.OperatorVariableAssign)
-    fun visit(node: ASTNode.Block)
-    fun visit(node: ASTNode.Expression.Binary.Plus)
-    fun visit(node: ASTNode.Expression.Binary.Sub)
-    fun visit(node: ASTNode.Expression.Binary.Mult)
-    fun visit(node: ASTNode.Expression.Binary.Div)
-    fun visit(node: ASTNode.Expression.Binary.Equals)
-    fun visit(node: ASTNode.Expression.Binary.NotEquals)
-    fun visit(node: ASTNode.Expression.Binary.GreaterEquals)
-    fun visit(node: ASTNode.Expression.Binary.Greater)
-    fun visit(node: ASTNode.Expression.Binary.Less)
-    fun visit(node: ASTNode.Expression.Binary.LessEquals)
-    fun visit(node: ASTNode.Expression.Binary.And)
-    fun visit(node: ASTNode.Expression.Binary.Or)
-    fun visit(node: ASTNode.Expression.Unary.Sub)
-    fun visit(node: ASTNode.Expression.Unary.Not)
-    fun visit(node: ASTNode.Expression.Unary.Increment)
-    fun visit(node: ASTNode.Expression.Unary.Decrement)
-    fun visit(node: ASTNode.Expression.LiteralExpr.IntLiteral)
-    fun visit(node: ASTNode.Expression.LiteralExpr.FloatLiteral)
-    fun visit(node: ASTNode.Expression.LiteralExpr.DoubleLiteral)
-    fun visit(node: ASTNode.Expression.LiteralExpr.IdentifierLiteral)
-    fun visit(node: ASTNode.Expression.LiteralExpr.FunctionCall)
+interface ASTNodeVisitor<T> {
+    fun visit(node: ASTNode.Class): T
+    fun visit(node: ASTNode.Constructor): T
+    fun visit(node: ASTNode.ConstructorVariableDeclaration): T
+    fun visit(node: ASTNode.FunctionDefinition): T
+    fun visit(node: ASTNode.FunctionParameter): T
+    fun visit(node: ASTNode.ForLoop): T
+    fun visit(node: ASTNode.WhileLoop): T
+    fun visit(node: ASTNode.IfStatement): T
+    fun visit(node: ASTNode.ElseStatements.ElseIfStatement): T
+    fun visit(node: ASTNode.ElseStatements.ElseStatement): T
+    fun visit(node: ASTNode.VariableDeclaration): T
+    fun visit(node: ASTNode.VariableAssign): T
+    fun visit(node: ASTNode.OperatorVariableAssign): T
+    fun visit(node: ASTNode.Block): T
+    fun visit(node: ASTNode.Expression.Binary.Plus): T
+    fun visit(node: ASTNode.Expression.Binary.Sub): T
+    fun visit(node: ASTNode.Expression.Binary.Mult): T
+    fun visit(node: ASTNode.Expression.Binary.Div): T
+    fun visit(node: ASTNode.Expression.Binary.Equals): T
+    fun visit(node: ASTNode.Expression.Binary.NotEquals): T
+    fun visit(node: ASTNode.Expression.Binary.GreaterEquals): T
+    fun visit(node: ASTNode.Expression.Binary.Greater): T
+    fun visit(node: ASTNode.Expression.Binary.Less): T
+    fun visit(node: ASTNode.Expression.Binary.LessEquals): T
+    fun visit(node: ASTNode.Expression.Binary.And): T
+    fun visit(node: ASTNode.Expression.Binary.Or): T
+    fun visit(node: ASTNode.Expression.Unary.Sub): T
+    fun visit(node: ASTNode.Expression.Unary.Not): T
+    fun visit(node: ASTNode.Expression.Unary.Increment): T
+    fun visit(node: ASTNode.Expression.Unary.Decrement): T
+    fun visit(node: ASTNode.Expression.LiteralExpr.IntLiteral): T
+    fun visit(node: ASTNode.Expression.LiteralExpr.FloatLiteral): T
+    fun visit(node: ASTNode.Expression.LiteralExpr.DoubleLiteral): T
+    fun visit(node: ASTNode.Expression.LiteralExpr.IdentifierLiteral): T
+    fun visit(node: ASTNode.Expression.LiteralExpr.FunctionCall): T
 }
 
-fun ASTNode.accept(visitor: ASTNodeVisitor) {
-    when (this) {
-        is ASTNode.Class -> visitor.visit(this)
-        is ASTNode.Constructor -> visitor.visit(this)
-        is ASTNode.ConstructorVariableDeclaration -> visitor.visit(this)
-        is ASTNode.FunctionDefinition -> visitor.visit(this)
-        is ASTNode.FunctionParameter -> visitor.visit(this)
-        is ASTNode.ForLoop -> visitor.visit(this)
-        is ASTNode.WhileLoop -> visitor.visit(this)
-        is ASTNode.IfStatement -> visitor.visit(this)
-        is ASTNode.ElseStatements.ElseIfStatement -> visitor.visit(this)
-        is ASTNode.ElseStatements.ElseStatement -> visitor.visit(this)
-        is ASTNode.VariableDeclaration -> visitor.visit(this)
-        is ASTNode.VariableAssign -> visitor.visit(this)
-        is ASTNode.OperatorVariableAssign -> visitor.visit(this)
-        is ASTNode.Block -> visitor.visit(this)
-        is ASTNode.Expression.Binary.Plus -> visitor.visit(this)
-        is ASTNode.Expression.Binary.Sub -> visitor.visit(this)
-        is ASTNode.Expression.Binary.Mult -> visitor.visit(this)
-        is ASTNode.Expression.Binary.Div -> visitor.visit(this)
-        is ASTNode.Expression.Binary.Equals -> visitor.visit(this)
-        is ASTNode.Expression.Binary.NotEquals -> visitor.visit(this)
-        is ASTNode.Expression.Binary.GreaterEquals -> visitor.visit(this)
-        is ASTNode.Expression.Binary.Greater -> visitor.visit(this)
-        is ASTNode.Expression.Binary.Less -> visitor.visit(this)
-        is ASTNode.Expression.Binary.LessEquals -> visitor.visit(this)
-        is ASTNode.Expression.Binary.And -> visitor.visit(this)
-        is ASTNode.Expression.Binary.Or -> visitor.visit(this)
-        is ASTNode.Expression.Unary.Sub -> visitor.visit(this)
-        is ASTNode.Expression.Unary.Not -> visitor.visit(this)
-        is ASTNode.Expression.Unary.Increment -> visitor.visit(this)
-        is ASTNode.Expression.Unary.Decrement -> visitor.visit(this)
-        is ASTNode.Expression.LiteralExpr.IntLiteral -> visitor.visit(this)
-        is ASTNode.Expression.LiteralExpr.FloatLiteral -> visitor.visit(this)
-        is ASTNode.Expression.LiteralExpr.DoubleLiteral -> visitor.visit(this)
-        is ASTNode.Expression.LiteralExpr.IdentifierLiteral -> visitor.visit(this)
-        is ASTNode.Expression.LiteralExpr.FunctionCall -> visitor.visit(this)
-    }
+fun <T> ASTNode.accept(visitor: ASTNodeVisitor<T>) = when (this) {
+    is ASTNode.Class -> visitor.visit(this)
+    is ASTNode.Constructor -> visitor.visit(this)
+    is ASTNode.ConstructorVariableDeclaration -> visitor.visit(this)
+    is ASTNode.FunctionDefinition -> visitor.visit(this)
+    is ASTNode.FunctionParameter -> visitor.visit(this)
+    is ASTNode.ForLoop -> visitor.visit(this)
+    is ASTNode.WhileLoop -> visitor.visit(this)
+    is ASTNode.IfStatement -> visitor.visit(this)
+    is ASTNode.ElseStatements.ElseIfStatement -> visitor.visit(this)
+    is ASTNode.ElseStatements.ElseStatement -> visitor.visit(this)
+    is ASTNode.VariableDeclaration -> visitor.visit(this)
+    is ASTNode.VariableAssign -> visitor.visit(this)
+    is ASTNode.OperatorVariableAssign -> visitor.visit(this)
+    is ASTNode.Block -> visitor.visit(this)
+    is ASTNode.Expression.Binary.Plus -> visitor.visit(this)
+    is ASTNode.Expression.Binary.Sub -> visitor.visit(this)
+    is ASTNode.Expression.Binary.Mult -> visitor.visit(this)
+    is ASTNode.Expression.Binary.Div -> visitor.visit(this)
+    is ASTNode.Expression.Binary.Equals -> visitor.visit(this)
+    is ASTNode.Expression.Binary.NotEquals -> visitor.visit(this)
+    is ASTNode.Expression.Binary.GreaterEquals -> visitor.visit(this)
+    is ASTNode.Expression.Binary.Greater -> visitor.visit(this)
+    is ASTNode.Expression.Binary.Less -> visitor.visit(this)
+    is ASTNode.Expression.Binary.LessEquals -> visitor.visit(this)
+    is ASTNode.Expression.Binary.And -> visitor.visit(this)
+    is ASTNode.Expression.Binary.Or -> visitor.visit(this)
+    is ASTNode.Expression.Unary.Sub -> visitor.visit(this)
+    is ASTNode.Expression.Unary.Not -> visitor.visit(this)
+    is ASTNode.Expression.Unary.Increment -> visitor.visit(this)
+    is ASTNode.Expression.Unary.Decrement -> visitor.visit(this)
+    is ASTNode.Expression.LiteralExpr.IntLiteral -> visitor.visit(this)
+    is ASTNode.Expression.LiteralExpr.FloatLiteral -> visitor.visit(this)
+    is ASTNode.Expression.LiteralExpr.DoubleLiteral -> visitor.visit(this)
+    is ASTNode.Expression.LiteralExpr.IdentifierLiteral -> visitor.visit(this)
+    is ASTNode.Expression.LiteralExpr.FunctionCall -> visitor.visit(this)
 }
--- RecursiveDescent/src/main/kotlin/com/luzon/rd/ast/VisitorGenerator.kt
+++ RecursiveDescent/src/main/kotlin/com/luzon/rd/ast/VisitorGenerator.kt
@@ -3,24 +3,27 @@ package com.luzon.rd.ast
 import com.luzon.utils.indent
 import kotlin.reflect.KClass
 
+fun main() {
+    VisitorGenerator.generate()
+}
+
 object VisitorGenerator {
     fun generate() {
         val lines = findNodes()
         val visitorSb = StringBuilder()
         val acceptSb = StringBuilder()
-        visitorSb.appendln("interface ASTNodeVisitor {")
-        acceptSb.appendln("fun ASTNode.accept(visitor: ASTNodeVisitor) {")
-        acceptSb.indent().appendln("when (this) {")
+        visitorSb.appendln("interface ASTNodeVisitor<T> {")
+        acceptSb.appendln("fun <T> ASTNode.accept(visitor: ASTNodeVisitor<T>) = when (this) {") // TODO: Make this = when (this) ?
 
         lines.forEach {
             val trimmed = it.trim()
 
-            visitorSb.indent().appendln("fun visit(node: $trimmed)")
-            acceptSb.indent(2).appendln("is $trimmed -> visitor.visit(this)")
+            visitorSb.indent().appendln("fun visit(node: $trimmed): T")
+            acceptSb.indent().appendln("is $trimmed -> visitor.visit(this)")
         }
 
         visitorSb.appendln("}")
-        acceptSb.indent().appendln("}").appendln("}")
+        acceptSb.appendln("}")
 
         println(visitorSb.toString())
         println(acceptSb.toString())
