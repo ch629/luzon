be791cd14c1141fa2f108b24a26d26855db102a7
ch629 <CharlieHowes97@outlook.com>
2019-02-24 19:22:06 +0000

    More work fixing the Lexer TokenTest for gradle builds.

--- Lexer/src/test/kotlin/TokenTest.kt
+++ Lexer/src/test/kotlin/TokenTest.kt
@@ -1,3 +1,4 @@
+
 import com.luzon.fsm.IFsm
 import com.luzon.lexer.Token.Keyword
 import com.luzon.lexer.Token.Literal
@@ -9,7 +10,10 @@ import org.jetbrains.spek.api.dsl.it
 
 object TokenTest : Spek({
     given("a token file") {
-        val fsm = TokenMachine.getFSM()
+        var fsm = TokenMachine.getFSM()
+        // TODO: Not sure why this happens, but for the gradle build, I need to copy it again for it to work correctly.
+        fsm = fsm.copy()
+
         it("should successfully convert to an IFsm") {
             fsm.isRunning shouldBe true
         }
@@ -18,14 +22,15 @@ object TokenTest : Spek({
             fsm.accept("for")
             fsm.isAccepting shouldBe true
             fsm.currentOutput.firstOrNull() shouldBe Keyword.FOR
+        }
 
+        it("should recognize additions to 'for' as a Literal.IDENTIFIER") {
             fsm.accept('T')
             fsm.isAccepting shouldBe true
             fsm.currentOutput.firstOrNull() shouldBe Literal.IDENTIFIER
         }
     }
 })
-
 
 private fun IFsm<Char>.accept(input: String) {
     input.forEach {
